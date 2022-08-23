d30103387b6dc954a5a4e8ce958882d78384d17d
Nache Matos <103865858+NacheMatos98@users.noreply.github.com>
2022-08-23 18:20:04 -0300

    Add files via upload

---
+++ Starbucks Website - Landing Page/assets/js/script.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+    window.document.querySelector('.starbucks').src = anything;
+}
+
+function changeCircleColor(color){
+    const circle = window.document.querySelector('.circle');
+    circle.style.background = color;
+}
